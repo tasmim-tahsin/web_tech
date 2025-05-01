1d292cab73b20c2a420418c62bb8d75cc95e3436
Md Tasmim Al Tahsin <tahsinniyan@gmail.com>
2025-05-01 21:03:34 +0600

    ui and form validation upgrade

--- form.js
+++ form.js
@@ -3,10 +3,14 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
+// const location = document.getElementById('location');
+// const zip = document.getElementById('zip');
+const checkbox = document.getElementById('agree');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
     validateInputs();
+        
 });
 
 const setError = (element, message) => {
@@ -31,41 +35,83 @@ const isValidEmail = email => {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
+const isValidUsername = username => {
+    const re = /^[a-zA-Z][a-zA-Z0-9._]{2,19}$/;
+    return re.test(String(username).toLowerCase());
+}
 
 const validateInputs = () => {
     const usernameValue = username.value.trim();
     const emailValue = email.value.trim();
     const passwordValue = password.value.trim();
     const password2Value = password2.value.trim();
+    // const locationValue = location.value.trim();
+    // const zipValue = zip.value.trim();
 
-    if(usernameValue === '') {
+
+    let isValid = true;
+
+    if (usernameValue === '') {
         setError(username, 'Username is required');
+        isValid = false;
+    } else if (!isValidUsername(usernameValue)) {
+        setError(username, 'Provide a valid username');
+        isValid = false;
     } else {
         setSuccess(username);
     }
 
-    if(emailValue === '') {
+    if (emailValue === '') {
         setError(email, 'Email is required');
+        isValid = false;
     } else if (!isValidEmail(emailValue)) {
         setError(email, 'Provide a valid email address');
+        isValid = false;
     } else {
         setSuccess(email);
     }
 
-    if(passwordValue === '') {
+    if (passwordValue === '') {
         setError(password, 'Password is required');
-    } else if (passwordValue.length < 8 ) {
-        setError(password, 'Password must be at least 8 character.')
+        isValid = false;
+    } else if (passwordValue.length < 8) {
+        setError(password, 'Password must be at least 8 characters.');
+        isValid = false;
     } else {
         setSuccess(password);
     }
 
-    if(password2Value === '') {
+    if (password2Value === '') {
         setError(password2, 'Please confirm your password');
+        isValid = false;
     } else if (password2Value !== passwordValue) {
-        setError(password2, "Passwords doesn't match");
+        setError(password2, "Passwords don't match");
+        isValid = false;
     } else {
         setSuccess(password2);
     }
 
+    // if (locationValue === '') {
+    //     setError(location, 'Location is required');
+    //     isValid = false;
+    // } else {
+    //     setSuccess(location);
+    // }
+
+    // if (zipValue === '') {
+    //     setError(zip, 'Zip code is required');
+    //     isValid = false;
+    // } else {
+    //     setSuccess(zip);
+    // }
+
+    if (!checkbox.checked) {
+        isValid = false;
+    }
+
+    if (isValid) {
+        alert("Form submitted successfully!");
+        // form.submit();
+        form.reset();
+    }
 };
